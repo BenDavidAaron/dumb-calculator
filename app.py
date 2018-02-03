ff36aabda7a89e46fc0b079847f6cb8ab74d9d8e
The-Ben <ben@theaarons.com>
2018-02-03 14:09:50 -0800

    added Keras Model to app.py

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
 from flask import Flask,render_template, request
 import numpy as np
-import pandas as pd
-import pickle
+from keras.models import load_model
+
 
 app = Flask(__name__)
 app.vars = {}
 
-def findSum(a, b, estimator):
-	X = pd.DataFrame([[a,b]])
-	result = estimator.predict(X)
-	return result[0]
+app.vars['model'] = load_model('model-development/additionNet.h5')
+
+def findNeuralSum(a, b):
+    X = np.array([[a,b]])
+    #result = estimator.predict(X)
+    result = app.vars['model'].predict_classes(X)
+    return int(result[0])
+
+def findManualSum(a, b):
+    return a+b
 
 @app.route('/', methods =['GET', 'POST'])
 def index():
-	if request.method == 'GET':
-		return render_template('index.html', sum = 0, n1 = 0, n2 = 0)
+    if request.method == 'GET':
+        return render_template('index.html')
 
-	else:
-		if request.form['submit'] == 'L2 Regression':
-			predictor = pickle.load(open('model-development/predictor-lr.pkl', 'rb'))
-		elif request.form['submit'] == 'Gradient Descent':
-			predictor = pickle.load(open('model-development/predictor-gd.pkl', 'rb'))
-		else:
-			return render_template('index.html', sum = "couldn't load model", n1 = app.vars['n1'], n2 = app.vars['n2'])
-		try:
-			app.vars['n1'] = request.form['number1']
-			app.vars['n2'] = request.form['number2']
-			app.vars['sum'] = findSum(float(app.vars['n1']), float(app.vars['n2']), predictor)
-		except:
-			return render_template('index.html', sum = "%s + %s" % (app.vars['n1'], app.vars['n2']), n1 = app.vars['n1'], n2 = app.vars['n2'])
-		return render_template('index.html', sum = app.vars['sum'], n1 = app.vars['n1'], n2 = app.vars['n2'])
+    else:
+        app.vars['n1'] = request.form['number1']
+        app.vars['n2'] = request.form['number2']
+
+        app.vars['sumManual'] = findManualSum(app.vars['n1'], app.vars['n2'])
+
+        app.vars['sumNeural'] = findNeuralSum(app.vars['n1'], app.vars['n2'])
+
+        return render_template('index.html', )
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=33507, debug = True)
+    app.run(host='0.0.0.0', port=7000)
