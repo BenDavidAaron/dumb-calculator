e0747e2bfde4f3e2ee204e63e6d3c5dbe2792ae6
The-Ben <ben@theaarons.com>
2018-02-03 14:24:54 -0800

    Added time display to each computation

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template, request
 import numpy as np
 from keras.models import load_model
+import time
 
 
 app = Flask(__name__)
@@ -17,20 +18,31 @@ def findNeuralSum(a, b):
 def findManualSum(a, b):
     return a+b
 
+def timeFormat(elapsedSecs):
+    return "in %.2E sec." % elapsedSecs
+
 @app.route('/', methods =['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
 
     else:
-        app.vars['n1'] = request.form['number1']
-        app.vars['n2'] = request.form['number2']
+        app.vars['n1'] = int(request.form['number1'])
+        app.vars['n2'] = int(request.form['number2'])
 
-        app.vars['sumManual'] = findManualSum(app.vars['n1'], app.vars['n2'])
+        startTime = time.time()
+        sumManual = findManualSum(app.vars['n1'], app.vars['n2'])
+        timeManual = time.time()-startTime
 
-        app.vars['sumNeural'] = findNeuralSum(app.vars['n1'], app.vars['n2'])
+        startTime = time.time()
+        sumNeural = findNeuralSum(app.vars['n1'], app.vars['n2'])
+        timeNeural = time.time()-startTime
 
-        return render_template('index.html', )
+
+        timeNeuralDisplay = timeFormat(timeNeural)
+        timeManualDisplay = timeFormat(timeManual)
+
+        return render_template('index.html', sumManual = sumManual, sumNeural = sumNeural, timeManual = timeManualDisplay, timeNeural = timeNeuralDisplay, n1 = app.vars['n1'], n2 = app.vars['n2'])
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=7000)
